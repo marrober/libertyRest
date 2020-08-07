cc8d2b756cb08069cfbfc16e71e1524b01e17671
Mark Roberts <marrober@redhat.com>
2020-08-07 17:35:35 +0100

    Update create-tasks.sh, task-cache-pvc.yaml, and 5 more files...

--- build/create-tasks.sh
+++ build/create-tasks.sh
@@ -1,15 +1,11 @@
-oc create --save-config=true -f tasks/buildTask.yaml  
-oc create --save-config=true -f tasks/createRuntimeImageTask.yaml  
-oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml
-
-oc create --save-config=true -f pipelines/pipeline.yaml
-
 oc create --save-config=true -f image-streams/imageStream-liberty-rest-app.yaml 
-
-oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml
-oc create --save-config=true -f resources/imageStreamResource-liberty-rest-app.yaml  
-oc create --save-config=true -f resources/sourceCode-GitResource.yaml
-
-oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml
-
-oc create --save-config=true -f pipelineRun/pipelineRun.yaml
+oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml 
+oc create --save-config=true -f resources/imageStreamResource-intermediate.yaml 
+oc create --save-config=true -f resources/imageStreamResource-liberty-rest-app.yaml 
+oc create --save-config=true -f resources/imageStreamResource-G
+oc create --save-config=true -f resources/sourceCode-GitResource.yaml 
+oc create --save-config=true -f tasks/build.yaml 
+oc create --save-config=true -f tasks/clearBuildahRepo.yaml 
+oc create --save-config=true -f tasks/clearResources.yaml 
+oc create --save-config=true -f tasks/createRuntimeImage.yaml 
+oc create --save-config=true -f tasks/ocProcessDeploymentTemplate.yaml 
